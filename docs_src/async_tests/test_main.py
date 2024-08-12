a953e84f00088c5a1ce7eee26bc2d642f6061f8a
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2024-08-12 09:53:58 +0000

    ci:fix

--- docs_src/async_tests/test_main.py
+++ docs_src/async_tests/test_main.py
@@ -1,12 +1,11 @@
 import pytest
-from httpx import AsyncClient
-
+from httpx import AsyncClient, ASGITransport
 from .main import app
-
 
 @pytest.mark.anyio
 async def test_root():
-    async with AsyncClient(app=app, base_url="http://test") as ac:
+    transport = ASGITransport(app=app)
+    async with AsyncClient(transport=transport, base_url="http://test") as ac:
         response = await ac.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Tomato"}
