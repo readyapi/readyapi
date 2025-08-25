a8bcd6cd103956b8527b90b4bc163893b8377857
gitworkflows <bdkhulnasoft@gmail.com>
2025-08-24 20:36:18 -0700

    Create __init__.py (#236)
    * Create __init__.py
    
    Signed-off-by: gitworkflows <bdkhulnasoft@gmail.com>
    
    * 🎨 [pre-commit.ci] Auto format from pre-commit.com hooks
    
    * Create __init__.py
    
    Signed-off-by: gitworkflows <bdkhulnasoft@gmail.com>
    
    * 🎨 [pre-commit.ci] Auto format from pre-commit.com hooks
    
    * Create 01_basic_usage_example.py
    
    Signed-off-by: gitworkflows <bdkhulnasoft@gmail.com>
    
    * 🎨 [pre-commit.ci] Auto format from pre-commit.com hooks
    
    ---------
    
    Signed-off-by: gitworkflows <bdkhulnasoft@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ examples/mcp/01_basic_usage_example.py
@@ -0,0 +1,18 @@
+from readyapi_mcp import ReadyApiMCP
+
+from examples.shared.apps.items import app  # The ReadyAPI app
+from examples.shared.setup import setup_logging
+
+setup_logging()
+
+# Add MCP server to the ReadyAPI app
+mcp = ReadyApiMCP(app)
+
+# Mount the MCP server to the ReadyAPI app
+mcp.mount_http()
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
